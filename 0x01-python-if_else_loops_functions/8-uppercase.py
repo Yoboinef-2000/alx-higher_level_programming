b52cf2ca91c88ce6e1c82be0c5f672f415664772
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-02 17:08:00 -0800

    Love Ayn Rand

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ""
     for char in s:
         if ord('a') <= ord(char) <= ord('z'):
             uppercase_char = chr(ord(char) - ord('a') + ord('A'))
-            print(uppercase_char, end="")
+            result += uppercase_char
         else:
-            print(char, end="")
-    print()
+            result += char
+
+    print("{}".format(result))
