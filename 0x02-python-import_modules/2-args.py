4ec6d0799ebdde7e5cdd7507708959ce2758ea13
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-04 07:57:01 -0800

    Real foul yute life Geoffery

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,17 @@
 
 import sys
 
-args_count = len(sys.argv) - 1
+if __name__ == "__main__":
+    argsnum = len(sys.argv)
 
-if args_count == 0:
-    print(".")
-else:
-    print("{} argument{}:".format(args_count, 's' if args_count > 1 else ''))
-    for i in range(1, args_count + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    if argsnum == 1:
+        print(".")
+    else:
+        if argsnum == 2:
+            print("1 argument:")
+        elif argsnum > 2:
+            print("{} arguments:".format(argsnum - 1))
+
+        for i in range(argsnum):
+            if i != 0:
+                print("{}: {}".format(i, sys.argv[i]))
