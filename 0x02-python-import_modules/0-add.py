2d863711aa8dae6a1b79629bf261b58fb89bea70
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-04 06:51:13 -0800

    BYE BYE MS.AMERICAN PIE

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,6 @@ import add_0
 if __name__ == "__main__":
     a = 1
     b = 2
+    summ = add_0.add(a, b)
 
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, summ))
