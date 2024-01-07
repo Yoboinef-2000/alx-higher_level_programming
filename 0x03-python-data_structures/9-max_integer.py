6b237c488b81809c84bb2427a4be1561cf5bd536
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-07 07:56:09 -0800

    Aint no such thing as halfway crooks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return(None)
+        return (None)
     else:
         maxx = my_list[0]
         for number in my_list:
             if number > maxx:
                 maxx = number
-        return(maxx)
+        return (maxx)
