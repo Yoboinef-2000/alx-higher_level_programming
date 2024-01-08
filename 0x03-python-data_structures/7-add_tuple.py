e707f2643b6af7be2bd90b26d9aca6c63260dac1
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-08 04:02:03 -0800

    See i never took you for the popping type

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        if (tuple_a[0] == None):
-            tuple_a[0] = 0
-        else:
-            tuple_a[1] = 0
-    if len(tuple_b) < 2:
-        if (tuple_b[0] == None):
-            tuple_b[0] = 0
-        else:
-            tuple_b[1] = 0
-    newtuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return (newtuple)
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    newtuple = (a1 + b1, a2 + b2)
+    return newtuple
