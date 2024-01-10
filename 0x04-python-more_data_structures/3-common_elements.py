536d2c7fbb41c14bcc6b068e59612609e52fe180
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-10 12:30:16 -0800

    Maybe I am foolish, maybe I am blind

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,10 +7,10 @@ def common_elements(set_1, set_2):
     sett1 = list(set_1)
     sett2 = list(set_2)
 
-    while (i < len(sett1)):
-        while (j < len(sett2)):
+    while i < len(sett1):
+        while j < len(sett2):
             if (sett1[i] == sett2[j]):
                 newList.append(sett1[i])
             j += 1
         i += 1
-    return (newList)
+    return newList
