c7ac429f8deba9b23b95dbf7ab7b80149d22c925
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-10 16:40:15 -0800

    Kokob semay temelesi lbey ahdiyo

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,15 +2,19 @@
 
 def common_elements(set_1, set_2):
     i = 0
-    j = 0
-    newList = []
-    sett1 = list(set_1)
-    sett2 = list(set_2)
 
-    while i < len(sett1):
-        while j < len(sett2):
-            if (sett1[i] == sett2[j]):
-                newList.append(sett1[i])
-            j += 1
-        i += 1
-    return newList
+    if set_1 is None or set_2 is None:
+        return set()
+    else:
+        newList = []
+        sett1 = list(set_1)
+        sett2 = list(set_2)
+
+        while (i < len(sett1)):
+            j = 0
+            while (j < len(sett2)):
+                if (sett1[i] == sett2[j]):
+                    newList.append(sett1[i])
+                j += 1
+            i += 1
+        return newList
