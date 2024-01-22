de682ea9268dcea24245bc60ec49f0ddd32148f7
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-22 14:27:02 -0800

    Anita Max Wynn

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    count = 0
+    try:
+        while(i < x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+            except (TypeError, ValueError):
+                pass
+            i = i + 1
+    except (IndexError):
+        pass
+    print()
+    return (count)
