66e874855882d3f6a5f4a4cf5ad9ff26398871c0
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-22 14:30:22 -0800

    Everything come back around full circle, why do the lies sound pleasant but the truth hurtful?

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except (TypeError, ValueError):
         pass
         return False
