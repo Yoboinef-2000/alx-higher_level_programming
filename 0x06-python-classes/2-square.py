2be28fc4cbda7e58547a853960121e3fda707d31
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-23 07:34:39 -0800

    The screams all sound the same

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This is a square class."""
+
+
+class Square:
+    """This is a square class."""
+
+    def __init__(self, size=0):
+        """Initialize a new instance of the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = int(size)
