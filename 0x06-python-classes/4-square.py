15ffc1c3666cde824e309564f910a00292181427
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-23 08:00:09 -0800

    ooo Yeah, SUch a long time

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This is a square class."""
+
+
+class Square:
+    """This is a square class."""
+
+    def __init__(self, size=0):
+        """Initialize a new instance of the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """Area of the square."""
+        return (self.__size * self.__size)
+
+    def size(self):
+        """Getter."""
+        return (self.__size)
+
+    def size(self, value):
+        """Setter."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
