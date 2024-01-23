6a37100acf7624609a88ebe1e7d2f72db7022a27
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-23 07:05:03 -0800

    Bye Bye Miss American Pie

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+This is a sqaure class that will have
+a private instance attribute called size
+"""
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
