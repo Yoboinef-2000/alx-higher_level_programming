5d5e3e667e00b338777253f95ddb1547475f3c07
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-01-23 06:26:18 -0800

    I keep on falling into you

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+    - None at the moment, as this is an empty class definition.
+
+    Methods:
+    - None yet, as this is just a basic structure.
+    """
     pass
