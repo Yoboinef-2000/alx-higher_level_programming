0240d1c1481c377e5126a82a14cc9244ba2490ad
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-02 18:12:08 -0800

    Them boi Paigon, i cant stand them

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    """Return a matrix divided by div."""
-    divMatrix = []
-    for i in matrix:
-        if not isinstance(i, (int, float)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        j = 0
-        for j in range(len(i)):
-            if not isinstance(j, (int, float)):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    result_matrix = [[round(num / div, 2) for num in row] for row in matrix]
 
+    return result_matrix
