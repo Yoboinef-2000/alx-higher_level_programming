bc588bed4f582639bdeb371e6f98815daec68e34
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-05 14:23:29 -0800

    Why do i crumble quickly?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""I am forced to have this line."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return true if obj is instance of a_class.
+
+    Return false otherwise.
+    """
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
