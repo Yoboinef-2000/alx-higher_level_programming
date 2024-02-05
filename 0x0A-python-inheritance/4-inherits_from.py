617b943e327da9cdb96f4eb46fdc7e9623677003
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-05 15:52:18 -0800

    Washannein

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""I have to include this line."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return true if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    Return false otherwise.
+    """
+    return (issubclass(type(obj), a_class))
