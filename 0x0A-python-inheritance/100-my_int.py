d3d3b947fd2480f4992622d607e8a0aeb102f872
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-05 18:27:47 -0800

    Im just saying you could do better.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,4 +6,10 @@
 class MyInt(int):
     """This class inherits from the int class."""
 
+    def __eq__(self, other):
+        """Override the == operator."""
+        return super().__ne__(other)
 
+    def __ne__(self, other):
+        """Override the != operator."""
+        return super().__eq__(other)
