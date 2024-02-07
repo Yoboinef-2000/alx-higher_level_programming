5beae6b13e16970283a75f6246bcf1a2bd8cd906
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-06 18:38:47 -0800

    Hey Mr.Carter, tell me where have you been

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,11 +7,17 @@ def pascal_triangle(n):
     """Return a list of lists of integers representing the
     Pascal’s triangle of n."""
     if (n <= 0):
-        return {}
-    else:
-        pascallist = [] * n
-        i = 0
-        while (i < n/2):
-            pascallist[i] = 1 + i
-            pascallist[n - i] = 
+        return []
+    pascallist = []
+    for i in range(n):
+        row = [1]
 
+        if i > 0:
+            rrow = pascallist[i - 1]
+            for j in range(1, i):
+                row.append(rrow[j - 1] + rrow[j])
+
+            row.append(1)
+        pascallist.append(row)
+
+    return (pascallist)
