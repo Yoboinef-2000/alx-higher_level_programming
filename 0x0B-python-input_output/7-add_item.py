c8b741d5f2abe3f4a0576e422915d99c9f64eb5a
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-06 18:08:02 -0800

    Fkri teasiru.

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Import importlib."""
+import importlib
+
+"""Import sys."""
+import sys
+
+"""I have to include this line."""
+
+
+saveJson = importlib.import_module('5-save_to_json_file')
+loadJson = importlib.import_module('6-load_from_json_file')
+
+def main():
+    """Main function."""
+    data = loadJson.load_from_json_file("add_item.json")
+    data.extend(sys.argv[1:])
+    saveJson.save_to_json_file(data, "add_item.json")
