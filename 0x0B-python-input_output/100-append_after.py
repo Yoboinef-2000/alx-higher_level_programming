1e4cedb4e871697119bd3974972616d45fd47100
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-06 18:31:57 -0800

    Hey Mr.Carter, tell me where have you been

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""I have to include this line."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert a line of text after each line containing a specific string."""
+    with open(filename, 'r') as file:
+            lines = file.readlines()
+    
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
