fc7ca1cbe9cc94f2dc8db0bc9e69996fdaad5560
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-07 16:53:00 -0800

    I wont bite unless you like.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
 
     Return false otherwise.
     """
-    return (issubclass(type(obj), a_class))
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
