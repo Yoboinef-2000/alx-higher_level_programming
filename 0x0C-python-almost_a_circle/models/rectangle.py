8e60aca6ce5c7c0c400b598ef7d693e5b4125363
Neftalem <nhagos2000@gmail.com>
2024-02-12 15:35:54 -0800

    Hey Mr.Carter, tell me where have you been.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,7 +40,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set width."""
-        if type(value) is not int:
+        if type(value) is not int or not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -49,7 +49,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set height."""
-        if type(value) is not int:
+        if type(value) is not int or not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -58,19 +58,19 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set x."""
-        if type(value) is not int:
+        if type(value) is not int or not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         """Set y."""
-        if type(value) is not int:
+        if type(value) is not int or not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
