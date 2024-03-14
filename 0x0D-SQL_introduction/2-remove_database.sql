6ebd1191879d12720ee90284f452c1a611f3ac69
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-03-14 04:16:02 -0700

    Shalipopi.

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- This script deletes a database named hbtn_0c_0 in your MySQL server.
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- This script will list all the tables of a database in your MySQL server.
+SHOW TABLES;
