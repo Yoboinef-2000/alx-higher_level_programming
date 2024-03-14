13f05dcfb32968ea04cd1d38cca5332246cb8aa7
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-03-14 14:34:39 -0700

    We be the infamous, you heard of us, official Queensbridge ........

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- This script lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 SELECT tv_show_genres.genre_id AS genre,
-       COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+       COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 GROUP BY tv_show_genres.genre_id
 HAVING COUNT(tv_show_genres.show_id) > 0
