758183ea604d3036426bcf1bd12712ff03bd437a
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-03-14 04:57:57 -0700

    Ka Valungu

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- This script displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT city, AVG(temperature * 9/5 + 32) AS average_temperature_fahrenheit
+SELECT city, AVG(te2) AS average_temperature_fahrenheit
 FROM your_table_name
 GROUP BY city
 ORDER BY average_temperature_fahrenheit DESC;
