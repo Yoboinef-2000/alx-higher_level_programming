c38a2296c3fa0ff91643f092803d3dd60d272338
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-03-13 18:24:50 -0700

    Geshe!

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
--- This script lists all cities contained in the database hbtn_0d_usa.
+-- This script lists all cities
+-- contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities, states
 WHERE cities.state_id = states.id
