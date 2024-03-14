2c9f3a3672795efcf58bac845195497d9881dc05
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-03-14 14:20:49 -0700

    We be the infamous, you heard of us, official QueensBridge ......

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,7 +4,7 @@ FROM tv_genres
 WHERE tv_genres.id NOT IN (
     SELECT tv_show_genres.genre_id
     FROM tv_show_genres
-    JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
     WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
