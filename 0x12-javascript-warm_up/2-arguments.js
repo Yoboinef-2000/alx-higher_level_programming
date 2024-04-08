7e6d80814dc25720f82fc61213f2194ddb2ebfa3
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-08 23:05:59 +0300

    Huntin Wabbitz

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const numArgs = process.argv.length;
-if (numArgs < 2) {
+if (numArgs <= 2) {
   console.log('No argument');
 } else if (numArgs === 3) {
   console.log('Argument found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args[0] === undefined) {
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
