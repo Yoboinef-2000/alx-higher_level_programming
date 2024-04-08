3b3708cd3f34ba526083c59eba5d50e1dcdaf724
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-08 23:48:49 +0300

    Huntin Wabbitz

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (numargs <= 3) {
   console.log(0);
 } else {
   let highestnumber = Number(allargs[2]);
-  let secondhighestnumber;
+  let secondhighestnumber = Number.NEGATIVE_INFINITY;
   for (let i = 3; i < numargs; i++) {
     if (highestnumber < Number(allargs[i])) {
       secondhighestnumber = highestnumber;
