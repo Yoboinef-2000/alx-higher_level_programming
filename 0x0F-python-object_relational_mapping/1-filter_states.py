c4da8eae36e625803e5a9ca5386a4ec68e5732f8
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-12 17:48:56 +0300

    Clandestina

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,22 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+
     theUsername = sys.argv[1]
     thePassword = sys.argv[2]
     theDatabase = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port="3306",
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=theUsername, passwd=thePassword,
                          db=theDatabase, charset="utf8")
     dbcur = db.cursor()
     dbcur.execute("""SELECT * FROM states WHERE name
-                  LIKE BINARY 'N%' ORDER BY states.id ASC""")
+                LIKE BINART 'N%' ORDER BY states.id ASC""")
+
     everything = dbcur.fetchall()
     for stts in everything:
         print(stts)
 
     dbcur.close()
     db.close()
+    
