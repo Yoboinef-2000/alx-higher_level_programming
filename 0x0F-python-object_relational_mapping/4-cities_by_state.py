bdc4a519712d5191ca2421ea5c606d26b0fe5da7
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-13 17:16:06 +0300

    Clandestina.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Import the sys and MySQLdb modules."""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    theUsername = sys.argv[1]
+    thePassword = sys.argv[2]
+    theDatabase = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=theUsername, passwd=thePassword,
+                         db=theDatabase, charset="utf8")
+    dbcur = db.cursor()
+    dbcur.execute("""SELECT * FROM
+                cities ORDER BY cities.id ASC;""")
+
+    everything = dbcur.fetchall()
+    for stts in everything:
+        print(stts)
+
+    dbcur.close()
+    db.close()
