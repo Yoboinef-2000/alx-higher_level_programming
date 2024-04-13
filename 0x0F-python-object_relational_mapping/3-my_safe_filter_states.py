4ebdd8ce63a91ecabae12567a2c8041a8f19cb02
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-13 16:43:51 +0300

    Clandestina

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Import the sys and MySQLdb modules."""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    theUsername = sys.argv[1]
+    thePassword = sys.argv[2]
+    theDatabase = sys.argv[3]
+    theUserInput = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=theUsername, passwd=thePassword,
+                         db=theDatabase, charset="utf8")
+    dbcur = db.cursor()
+    dbcur.execute("SELECT * FROM states WHERE name  = %s\
+                ORDER BY states.id ASC", (theUserInput,))
+
+    everything = dbcur.fetchall()
+    for stts in everything:
+        print(stts)
+
+    dbcur.close()
+    db.close()
