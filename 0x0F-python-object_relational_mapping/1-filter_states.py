57b30bb42ab1ea4716ee7c98d37ad134c5d49353
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-15 02:05:34 +0300

    Deliverance

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""lists all the states that are like a given name from the database"""
-import MySQLdb
-import sys
 
+"""Import the sys and MySQLdb modules."""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
-    match = "N%"
-    cur.execute("""SELECT * FROM states WHERE BINARY name
-                LIKE %s ORDER BY id""", (match, ))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
-    
+
+    theUsername = sys.argv[1]
+    thePassword = sys.argv[2]
+    theDatabase = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port="3306",
+                         user=theUsername, passwd=thePassword,
+                         db=theDatabase, charset="utf8")
+    dbcur = db.cursor()
+    dbcur.execute("""SELECT * FROM states WHERE BINARY name
+                  LIKE %s ORDER BY id""", ("N%", ))
+    everything = dbcur.fetchall()
+    for stts in everything:
+        print(stts)
+
+    dbcur.close()
+    db.close()
