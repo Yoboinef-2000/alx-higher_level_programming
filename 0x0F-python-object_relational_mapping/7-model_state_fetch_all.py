857781f05c486af61145440d6572d17be69b62b0
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-15 03:59:24 +0300

    Clandestina.

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Import statements."""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import Session
+
+if "__name__" == "__main__":
+    theUsername = sys.argv[1]
+    thePassword = sys.argv[2]
+    theDatabase = sys.arg[3]
+
+    engine - create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(theUsername, thePassword,
+                                   theDatabase), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    sesh = Session(engine)
+
+    for stateObj in sesh.query(State).order_by(State.id).all():
+        print(stateObj.id + ": " + stateObj.name)
+    sesh.close()
---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Import statements."""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import Session
+
+if "__name__" == "__main__":
+    theUsername = sys.argv[1]
+    thePassword = sys.argv[2]
+    theDatabase = sys.arg[3]
+
+    engine - create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(theUsername, thePassword,
+                                   theDatabase), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    sesh = Session(engine)
+
+    firstState = sesh.query(State).order_by(State.id).first()
+    if firstState is None:
+        print("Nothing")
+    else:
+        print(firstState.id + ": " + firstState.name)
+
+    sesh.close()
