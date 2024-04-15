c1db29792b28a56ecf35ad65f22f722b4bf3517b
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-15 04:13:20 +0300

    Nocta

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if "__name__" == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(theUsername, thePassword,
-                                   theDatabase))
+                                   theDatabase), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
