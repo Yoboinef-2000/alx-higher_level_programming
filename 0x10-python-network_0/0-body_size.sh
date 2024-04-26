db3e1d58dd1445656262b1f2e0fcf19b474e3146
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-26 04:26:11 +0300

    All your Bash scripts should be exactly 3 linew long.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-
-theURL="$1"
-curl -s -o /dev/null -w "%{size_download}\n" "$theURL"
-
-# -s: Silent mode - Option that operates silently without showing the progress information
-# -w : Output format
-# {size_download} : displays the size of the downloaded content in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
