805e4f22e9d37106df81c96c0f8583670f2fd31d
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-26 04:28:45 +0300

    Oooo, we are half way there, Oooo, living on a prayer.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of th response
-
-theURL="$1"
-curl -s -X GET -L "$theURL"
-
-# -X : As mentioned in the quiz and the course material for it, this option
-# defines the HTTP method used. I need to put that thorugh my thick head.
-# I think I will remember it if I start using it often.
-# Which means I wont remember it ever.
-
-# -L : Again, as mentioned in the quiz and the curl video I watched for it, this option
-# is for following redirects.
-# But ask me again, will I ever remember this flag?
-# Yeah you guessed right!
-# ;)
+curl -s -X GET -L "$1"
