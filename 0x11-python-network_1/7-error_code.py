b5fd558e83f13066decc730120e1328eba0bbf2c
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-27 05:28:26 +0300

    Emiliana.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This Python Script takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    theURL = sys.argv[1]
+    readURL = requests.get(theURL)
+    if readURL.status_code >= 400:
+        print("Error code:", readURL.status_code)
+    else:
+        print(readURL.text)
