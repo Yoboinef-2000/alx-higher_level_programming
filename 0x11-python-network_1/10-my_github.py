26eae5a9924ca60b87b047e6f4c9ae1854290ba4
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 02:53:39 +0300

    Like Kante your skill is precious.

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-This Python script takes your GitHub credentials (username and password) and uses
-the GitHub API to display your id.
+This Python script takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
 """
 
 import sys
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     theRequest = requests.get('https://api.github.com/user',
-                     auth=(sys.argv[1], sys.argv[2]))
+                              auth=(sys.argv[1], sys.argv[2]))
     if theRequest.status_code >= 400:
         print('None')
     else:
