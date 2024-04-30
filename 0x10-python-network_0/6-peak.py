5ac1c6b0178e04dde3428bfa0b6f832c94c00c93
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 01:46:28 +0300

    Oooo, we are half way there, Oooo, living on a prayer.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,14 @@
 def find_peak(list_of_integers):
     """Finds the peak in a list of unsorted integers."""
     if list_of_integers:
-        max = list_of_integers[0]
-        for i in list_of_integers:
-            if i > max:
-                max = i
+        low = 0
+        high = len(list_of_integers) - 1
 
-        return (max)
+        while low < high:
+            mid = (low + high) // 2
+
+            if list_of_integers[mid] < list_of_integers[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid
+        return list_of_integers[low]
