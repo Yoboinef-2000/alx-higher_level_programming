1df149e322a13949eaa9fb16cc13c08d86e797d2
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-01 03:20:59 +0300

    Like Kante your skill is precious.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This Python script take 2 arguments in order to solve this challenge:
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to check arguments passed to the script (number or type)
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    gitHubRepo = sys.argv[1]
+    gitHubOwner = sys.argv[2]
+    theRequest = requests.get('https://api.github.com/repos/{}/{}/commits'
+                              .format(gitHubOwner, gitHubRepo))
+
+    if theRequest >= 400:
+        print('None')
+    else:
+        for ola in (c for c in theRequest.json()[:10]):
+            print("{}: {}".format(ola['sha'], ola['commit']['author']['name']))
