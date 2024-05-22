5774706d3aa6a5e39c4bb88021c6beb54134a573
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-23 02:20:39 +0300

    Wicked Games.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,18 +6,18 @@ const request = require('request');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get(url, function(error, response, body) {
+request.get(url, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
-  
+
   if (response.statusCode !== 200) {
     console.error(response.statusCode);
     return;
   }
-  
-  fs.writeFile(filePath, body, 'utf8', function(err) {
+
+  fs.writeFile(filePath, body, 'utf8', function (err) {
     if (err) {
       console.error(err);
     } else {
