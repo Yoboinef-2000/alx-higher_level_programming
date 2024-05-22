dd2edf6fea39e0696a7bc059b6eeb32051f2e208
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-22 05:29:52 +0300

    Been procastinating a lot lately.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
+    console.error(error);
   } else {
     console.log('code:', response.statusCode);
   }
